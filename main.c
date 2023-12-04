1e7ed495394eb01b74eedd7a2357025781db9a95
Ivan Butov <123304942+IB004@users.noreply.github.com>
2023-12-04 10:37:46 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "ring.h"
 #include "tokenizer.h"
 
-void ast_print(struct AST ast) { print_ast(stdout, &ast); }
+void ast_print(struct AST *ast) { print_ast(stdout, ast); }
 void token_print(struct token token) { printf("%s(%" PRId64 ")", TOKENS_STR[token.type], token.value); }
 
 
@@ -17,7 +17,7 @@ DEFINE_RING_PRINT(oper, token_print)
 
 DECLARE_RING(ast, struct AST*)
 DEFINE_RING(ast, struct AST*)
-//DEFINE_RING_PRINT(ast, ast_print)
+DEFINE_RING_PRINT(ast, ast_print)
 
 //DECLARE_RING(token, struct token)	//declared in "tokenizer.h"
 DEFINE_RING(token, struct token)
@@ -60,13 +60,14 @@ static void exec_top(struct ring_oper* opers, struct ring_ast* asts){
 	struct AST* new;
 	if (token_is_unop(top)){
 		struct AST* operand = ring_ast_pop(&asts);
-		if (operand->type != AST_LIT){ exit_error(-1, "Incorrect expression!"); }
+		if (operand == NULL){ exit_error(-1, "Incorrect expression: no operand."); }
 		new = unop(TOKEN_TO_AST[top.type], operand);
 	}
 	else{
 		struct AST* right = ring_ast_pop(&asts);
+		if (right == NULL){ exit_error(-1, "Incorrect expression: no args!"); }
 		struct AST* left = ring_ast_pop(&asts);
-		if (right->type != AST_LIT || left->type != AST_LIT){ exit_error(-1, "Incorrect expression!"); }
+		if (left == NULL){ exit_error(-1, "Incorrect expression: need one more arg!"); }
 		new = binop(TOKEN_TO_AST[top.type], left, right);
 	}
 	ring_ast_push(&asts, new);
@@ -76,7 +77,7 @@ void exec_until(enum token_type const end_type, struct ring_oper* opers, struct 
 	while(1){
 		struct token top = ring_oper_last(opers);
 		if (top.type == end_type){ ring_oper_pop(&opers); break; }
-		if (top.type == TOK_END){ exit_error(-1, "Incorrect expression!"); }
+		if (top.type == TOK_END){ exit_error(-1, "Incorrect expression: rich end-token before expected!"); }
 		exec_top(opers, asts);
   }
 }
@@ -114,18 +115,19 @@ struct AST *build_ast(char *str)
   
   struct token end = {TOK_END, 0};
   struct ring_oper* opers = ring_oper_create(end);
-  struct ring_ast* asts = ring_ast_create(binop(BIN_PLUS, NULL, NULL));
+  struct ring_ast* asts = ring_ast_create(NULL);
   
   while(ring_token_first(tokens).type != TOK_END){
   	struct token next = ring_token_pop_top(&tokens);
-		token_print(next), printf("\n");
+		//token_print(next), printf("\n");
 		process_one_token(next, opers, asts);
-		ring_oper_print(opers), printf("\n");
+		//ring_oper_print(opers), printf("\n");
   }
   exec_until(TOK_END, opers, asts);
 
 
   ring_token_free(&tokens);
+  ring_oper_free(&opers);
 
   return ring_ast_pop(&asts);
 }
